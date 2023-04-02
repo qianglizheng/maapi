c03ba0e8e0f3126752ea6c0bf046a5df406b577a
小码哥 <1648785@qq.com>
2023-04-02 20:19:20 +0800

    1

--- app/admin/controller/Config.php
+++ app/admin/controller/Config.php
@@ -9,19 +9,37 @@ use think\facade\View;
 
 class Config
 {
+    /**
+     * key设置
+     */
     public function adminKeyConfig()
     {
         $data = AdminKeyConfig::find(1)->toArray();
         View::assign($data);
         return View::fetch('adminKeyConfig');
     }
+    /**
+     * 邮箱设置
+     */
     public function adminEmailConfig()
     {
         $data = AdminEmailConfig::find(1)->toArray();
         View::assign($data);
         return View::fetch('adminEmailConfig');
     }
-    public function smstop()
+    /**
+     * 接口设置
+     */
+    public function adminApiConfig()
+    {
+        $data = AdminEmailConfig::find(1)->toArray();
+        View::assign($data);
+        return View::fetch('adminApiConfig');
+    }
+    /**
+     * 插件 顶想云短信
+     */
+    public function addonsSmsTop()
     {
         $data = AdminEmailConfig::find(1)->toArray();
         View::assign($data);

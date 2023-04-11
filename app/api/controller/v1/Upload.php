93c9eea3787d375c9acefc4bfe9feafda9c56541
小码哥 <1648785@qq.com>
2023-04-11 12:13:34 +0800

    11

--- app/api/controller/v1/Upload.php
+++ app/api/controller/v1/Upload.php
@@ -2,7 +2,9 @@
 
 namespace app\api\controller\v1;
 
-class Upload
+use app\common\controller\Common;
+
+class Upload extends Common
 {
     public function upload($path)
     {
@@ -12,6 +14,12 @@ class Upload
 
         foreach ($files as $file) {
             $savename[] = \think\facade\Filesystem::putFile($path, $file);
+        }
+        $count = count($savename);
+        if ($count != 0) {
+            return $this->return_json($count, $savename, '文件上传成功');
+        } else {
+            return $this->return_json(0,[],'文件上传失败');
         }
     }
 }

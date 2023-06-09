387ea0ea8918709c730ddd598d19254226f6a235
小码哥 <1648785@qq.com>
2023-06-09 17:24:47 +0800

    1

--- app/api/route/app.php
+++ app/api/route/app.php
@@ -25,26 +25,32 @@ Route::get('v1/captcha/img', 'v1.captcha.ImgCode/sendImgCode');                 
 Route::post('v1/captcha/email', 'v1.captcha.EmailCode/sendEmailCode');           //邮件验证码
 Route::post('v1/captcha/mobile', 'v1.captcha.MobileCode/sendMobileCode');        //邮件验证码
 
+/**
+ * 公共路由 需要鉴权
+ */
+Route::group(function () {
+    //文件上传
+    Route::post('v1/upload/local', 'v1.upload.LocalUpload/upload');               //上传到本地
+})->middleware(\app\common\middleware\CheckToken::class)->middleware(\app\common\middleware\CheckAuth::class);
+
 
 /**
  * 管理后台路由
  */
-Route::post('admin/v1/login', 'admin.v1.Login/login');                           //管理员登录
+Route::post('admin/v1/login', 'admin.v1.Login/login');                          //管理员登录
 
 /**
- * 路由分组 验证token
+ * 路由分组 需要鉴权
  */
 Route::group(function () {
     //菜单栏
-    Route::get('admin/v1/menu', 'admin.v1.Menu/menu');            //管理后台菜单栏
-    Route::get('user/v1/menu', 'user.v1.Menu/menu');              //用户后台菜单栏
-    //文件上传
-    Route::post('v1/upload/local', 'v1.upload.LocalUpload/upload');                 //上传到本地
+    Route::get('admin/v1/menu', 'admin.v1.Menu/menu');                         //管理后台菜单栏
+    Route::get('user/v1/menu', 'user.v1.Menu/menu');                           //用户后台菜单栏
     //管理后台
-    Route::resource('admin/v1/email', 'admin.v1.config.Email');   //邮件设置
-    Route::resource('admin/v1/key', 'admin.v1.config.Key');       //密钥设置
-    Route::resource('admin/v1/api', 'admin.v1.config.Api');       //接口设置
-    Route::resource('admin/v1/base', 'admin.v1.config.Base');     //基本设置
+    Route::resource('admin/v1/email', 'admin.v1.config.Email');                //邮件设置
+    Route::resource('admin/v1/key', 'admin.v1.config.Key');                    //密钥设置
+    Route::resource('admin/v1/api', 'admin.v1.config.Api');                    //接口设置
+    Route::resource('admin/v1/base', 'admin.v1.config.Base');                  //基本设置
 
     Route::resource('admin/v1/users', 'admin.v1.Users');
 })->middleware(\app\common\middleware\CheckToken::class)->middleware(\app\common\middleware\CheckAuth::class);

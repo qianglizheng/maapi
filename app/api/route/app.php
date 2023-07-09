0113b90527fdf674d8b248938674a693feefc5f5
创梦流浪人 <1648785@qq.com>
2023-07-09 13:48:00 +0800

    1

--- app/api/route/app.php
+++ app/api/route/app.php
@@ -14,7 +14,7 @@ use think\facade\Route;
 //检查token中间件会检查token是否存在，所以不需要验证器再判断一遍
 
 /**
- * 公共验证码路由 不需要登录
+ * 公共接口 不需要登录
  */
 Route::get('v1/captcha/get-img', 'v1.captcha.ImgCode/getImg');                   //图片验证码
 Route::get('v1/captcha/img', 'v1.captcha.ImgCode/sendImgCode');                  //图片验证码信息
@@ -22,18 +22,19 @@ Route::post('v1/captcha/email', 'v1.captcha.EmailCode/sendEmailCode');          
 Route::post('v1/captcha/mobile', 'v1.captcha.MobileCode/sendMobileCode');        //邮件验证码
 
 /**
- * 公共接口路由 需要登录
+ * 公共接口 需要登录
  */
 Route::group(function () {
     //文件上传
     Route::post('v1/upload/local', 'v1.upload.LocalUpload/upload')->validate([
         'path'    =>    'require'
-    ]);               //上传到本地
+    ]);                                                                          //上传到本地
+    
 })->middleware(\app\common\middleware\CheckToken::class)->middleware(\app\common\middleware\CheckAuth::class);
 
 
 /**
- * 管理后台路由 不需要登录
+ * 管理后台接口 不需要登录
  */
 Route::post('admin/v1/login', 'admin.v1.Login/login')->validate([
     'username'    =>    'require',
@@ -41,20 +42,23 @@ Route::post('admin/v1/login', 'admin.v1.Login/login')->validate([
 ]);                                                                            //管理员登录
 
 /**
- * 路由分组 需要登录
+ * 管理后台接口 需要登录
  */
 Route::group(function () {
     //菜单栏
     Route::get('admin/v1/menu', 'admin.v1.Menu/menu');                         //管理后台菜单栏
     Route::get('user/v1/menu', 'user.v1.Menu/menu');                           //用户后台菜单栏
-    //管理后台
+    
+    //系统设置
     Route::resource('admin/v1/email', 'admin.v1.config.Email');                //邮件设置
     Route::resource('admin/v1/key', 'admin.v1.config.Key');                    //密钥设置
     Route::resource('admin/v1/api', 'admin.v1.config.Api');                    //接口设置
     Route::resource('admin/v1/base', 'admin.v1.config.Base');                  //基本设置
-
+    
+    //用户中心
     Route::resource('admin/v1/users', 'admin.v1.Users')->validate([
         'page'    =>    'require',
         'limit'   =>    'require',
-    ]);
+    ]);                                                                        //用户列表
+
 })->middleware(\app\common\middleware\CheckToken::class)->middleware(\app\common\middleware\CheckAuth::class);
